b6764aef3804d9ad02e94af5bd92d42f1f183457
Marko Djokic <marko1319@its.edu.rs>
2020-11-25 16:26:08 +0100

    Add project files.

---
+++ zadatak1/Automobil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadatak1
+{
+    class Automobil
+    {
+        private string proizvodjac;
+        private string model;
+        private double cena;
+        private string boja;
+        private int brzina;
+
+        public Automobil()
+        {
+            proizvodjac = " ";
+            model = " ";
+            cena = 0;
+            boja = " ";
+            brzina = 0;
+        }
+
+        public Automobil(string pr, string md, double c, string b, int br)
+        {
+            proizvodjac = pr;
+            model = md;
+            cena = c;
+            boja = b;
+            brzina = br;
+        }
+        public void Ispisi()
+        {
+            Console.WriteLine("Proizvodjac automobila je : " + proizvodjac);
+            Console.WriteLine("Model automobila je " + model);
+            Console.WriteLine("Cena automobila je " + cena + " eura.");
+            Console.WriteLine("Boja automobila je " + boja);
+            Console.WriteLine("Brzina automobila je " + brzina + " km/h.");
+        }
+
+        public string Proizvodjac
+        {
+            get { return proizvodjac; } set { proizvodjac = value; }
+        }
+        public string Model
+        {
+            get { return model; } set { model = value; }
+        }
+        public double Cena
+        {
+            get { return cena; } set { cena = value; }
+        }
+        public string Boja
+        {
+            get { return boja; } set { boja = value; }
+        }
+        public int Brzina
+        {
+            get { return brzina; } set { brzina = value; }
+        }
+    }
+}
---
+++ zadatak1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobil a1 = new Automobil();
+            a1.Proizvodjac = "Ford";
+            a1.Model = "Mustang";
+            a1.Cena = 26670;
+            a1.Boja = "Crvena";
+            a1.Brzina = 260;
+            Console.WriteLine("Podaci o pevom automobilu: ");
+            a1.Ispisi();
+            Console.WriteLine("===================================");
+            Automobil a2 = new Automobil("Opel", "Insignia", 25000, "Crvena", 240);
+            Console.WriteLine("Podaci o drugom automobilu: ");
+            a2.Ispisi();
+
+            Console.ReadLine();
+        }
+    }
+}
